72a208019021bdbdae7d3304ca2c8b204588b34e
donald-macritchie <>
2023-06-01 16:23:33 +0000

    add medium and hard functions. medium getting words with 7 letters and below, hard getting 10 letters and below

--- difficulty.py
+++ difficulty.py
@@ -1,6 +1,17 @@
 from hangman_words import words
 
+
 def easy():
     easy_word = [word for word in words if len(word) <= 4]
     print("You have selected the EASY level\n")
+
+
+def medium():
+    medium_word = [word for word in words if len(word) <= 7]
+    print("You have selected the MEDIUM level\n")
+
+
+def hard():
+    hard_word = [word for word in words if len(word) <= 10]
+    print("You have selected the HARD level\n")
 
