d6d31a03039611c98314db9aa00f4374278e8026
donald-macritchie <>
2023-06-01 11:50:52 +0000

    create play_game_function and create opening game monolugue

--- run.py
+++ run.py
@@ -1,19 +1,26 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+# SCOPE = [
+#     "https://www.googleapis.com/auth/spreadsheets",
+#     "https://www.googleapis.com/auth/drive.file",
+#     "https://www.googleapis.com/auth/drive"
+#     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('hangman_scores')
+# CREDS = Credentials.from_service_account_file('creds.json')
+# SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# SHEET = GSPREAD_CLIENT.open('hangman_scores')
 
-scores = SHEET.worksheet('latest_score')
 
-data = scores.get_all_values()
+# Create function for game
+# create function to have latest scores input into google sheet
+# create function to have highest scores input into google sheet
 
-print(data)
+def play_game():
+    print("Welcome to Hangman!\n")
+    print("Guess the letters to complete the word")
+    print("Try to guess the word in the least amount of guesses")
+    print("Dont let the man hang!\n")
+
+play_game()
