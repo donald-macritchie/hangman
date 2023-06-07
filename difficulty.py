4b9bb612f4ee2a9ae36588f70177e66786d14f89
donald-macritchie <>
2023-06-07 11:24:45 +0000

    Move game_difficulty funtion from difficulty.py into run.py

--- difficulty.py
+++ difficulty.py
@@ -1,28 +1,4 @@
 from hangman_words import words
 import random
 
-def game_difficulty():
-    print("Choose your difficulty level!\n")
-    print("EASY - Words with 4 letters or less")
-    print("MEDIUM - Words with 7 letters or less")
-    print("HARD - Words with 10 letters or less\n")
-    print("Please select a difficulty.")
-
-
-    user_difficulty = input("Type: 'easy', 'medium' or 'hard': \n").lower()
-    if user_difficulty == "easy":
-        easy_words = [word for word in words if len(word) <= 4]
-        computer_choice = random.choice(easy_words)
-        return computer_choice
-    elif user_difficulty == 'medium':
-        medium_words = [word for word in words if len(word) <= 7]
-        computer_choice = random.choice(medium_words)
-        return computer_choice
-    elif user_difficulty == 'hard':
-        hard_words = [word for word in words if len(word) <= 10]
-        computer_choice = random.choice(hard_words)
-        return computer_choice
-    elif user_difficulty != 'easy' and 'medium' and 'hard':
-        print("Invalid difficulty choice, please try again.\n")
-        game_difficulty()
 
--- run.py
+++ run.py
@@ -20,6 +20,32 @@ from difficulty import game_difficulty
 # Create function for game
 # create function to have latest scores input into google sheet
 # create function to have highest scores input into google sheet
+
+def game_difficulty():
+    print("Choose your difficulty level!\n")
+    print("EASY - Words with 4 letters or less")
+    print("MEDIUM - Words with 7 letters or less")
+    print("HARD - Words with 10 letters or less\n")
+    print("Please select a difficulty.")
+
+    user_difficulty = input("Type: 'easy', 'medium' or 'hard': \n").lower()
+    if user_difficulty == "easy":
+        easy_words = [word for word in words if len(word) <= 4]
+        computer_choice = random.choice(easy_words)
+        return computer_choice
+    elif user_difficulty == 'medium':
+        medium_words = [word for word in words if len(word) <= 7]
+        computer_choice = random.choice(medium_words)
+        return computer_choice
+    elif user_difficulty == 'hard':
+        hard_words = [word for word in words if len(word) <= 10]
+        computer_choice = random.choice(hard_words)
+        return computer_choice
+    elif user_difficulty != 'easy' and 'medium' and 'hard':
+        print("Invalid difficulty choice, please try again.\n")
+        game_difficulty()
+
+        
 print("Welcome to Hangman!\n")
 print("Guess the letters to complete the word")
 print("Try to guess the word in the least amount of guesses")
@@ -30,7 +56,7 @@ computer_choice = game_difficulty()
 print(computer_choice)
 
 
-def play_game():
+def play_game(computer_choice):
     game_over = False
     lives_remaining = 6
     wrong_letter = []
@@ -73,6 +99,9 @@ def play_game():
 
 
 def play_again():
+    """
+    Asks the user to play again if they complete the word
+    """
     continue_game = input("play again? y or n")
     if continue_game == "y":
         play_game(computer_choice)
@@ -81,6 +110,9 @@ def play_again():
 
 
 def score():
+    """
+    Keeps track of the user score
+    """
     score = 0
     score += 1
     print(f"You have a score of {score}")
@@ -88,10 +120,9 @@ def score():
         play_again()
 
 
-
 def main():
     # game_difficulty()
-    play_game()
+    play_game(computer_choice)
     score()
     
 
